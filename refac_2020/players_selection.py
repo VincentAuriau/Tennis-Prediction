2da945a867010b36729304baa96a18cec7f86f45
VincentAuriau <auriau.vincent@gmail.com>
2020-05-13 22:37:44 +0200

    FIX: few fixes

--- refac_2020/players_selection.py
+++ refac_2020/players_selection.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 import ast
+import sys
+
+sys.path.append('../')
+from Player import Player
+import pickle
 
 
 df_all = pd.read_csv('../Data/atp_players.csv', names=['id', 'LastName', 'SurName', 'Hand', 'BirthDate', 'Country'])
@@ -15,6 +20,7 @@ print(df_2019.columns)
 
 IDs_to_keep = []
 Names = []
+id_to_player = {}
 for index, row in df_all.iterrows():
     date = row['BirthDate']
     if date is not 'nan':
@@ -30,6 +36,17 @@ for index, row in df_all.iterrows():
                     IDs_to_keep.append(row['id'])
                     Names.append(row_19['name'].iloc[0])
 
+                    name = str(row['LastName']) + ' ' + str(row['SurName'])
+                    born_year = date
+                    country = str(row['Country'])
+                    player = Player(name, born_year, country, id)
+                    player.hand = row['Hand']
+                    id_to_player[id] = player
+
 print(Names)
 np.save('IDs_2019.npy', np.array(IDs_to_keep))
+print(id_to_player)
 
+with open('2019_players', 'wb') as file:
+    my_pickler = pickle.Pickler(file)
+    my_pickler.dump(id_to_player)
