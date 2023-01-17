d5062fb0279d4c6c545fb8f35fe71e5a7ada336a
VincentAuriau <auriau.vincent@gmail.com>
2023-01-17 23:34:47 +0100

    ADD: few details in example

--- examples/data_loading.py
+++ examples/data_loading.py
@@ -5,5 +5,8 @@ sys.path.append("../")
 
 from data.data_loader import matches_data_loader
 
-data_df  = matches_data_loader(path_to_data="../submodules/tennis_atp",
+data_df = matches_data_loader(path_to_data="../submodules/tennis_atp",
                                path_to_cache="../cache")
+
+print(data_df.head())
+print(data_df.shape)
