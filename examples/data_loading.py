06209e8060bb2402d9273688577e6c8c7d4d150a
VincentAuriau <auriau.vincent@gmail.com>
2023-01-18 04:20:08 +0100

    update example

--- examples/data_loading.py
+++ examples/data_loading.py
@@ -8,7 +8,8 @@ from data.data_loader import matches_data_loader
 data_df = matches_data_loader(path_to_data="../submodules/tennis_atp",
                                path_to_cache="../cache",
                               flush_cache=True,
-                              keep_values_from_year=2000)
+                              keep_values_from_year=2022,
+                              get_match_statistics=True)
 
 print(data_df.head())
 print(data_df.shape)
