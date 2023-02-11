09fb0dfaa6145b7101179f2d815dbfa1e5ee8c44
VincentAuriau <auriau.vincent@gmail.com>
2023-02-11 19:53:27 +0100

    ADD: complete data loading example

--- examples/data_loading.py
+++ examples/data_loading.py
@@ -24,9 +24,11 @@ print(data_df.shape)
 categories = [1, 10, 50, 100, 300, 1000, 9999]
 
 best_ranked_player_win_percentage = []
+categories_number_of_matches = []
 
 for cat_1 in range(len(categories) - 1):
     lines = []
+    nb_matches_lines = []
     for cat_2 in range(len(categories) - 1):
         sub_df = data_df.loc[data_df.Ranking_1 >= categories[cat_1]].loc[
             data_df.Ranking_1 < categories[cat_1 + 1]
@@ -46,7 +48,9 @@ for cat_1 in range(len(categories) - 1):
         else:
             best_player_w_p = 0
         lines.append(best_player_w_p)
+        nb_matches_lines.append(len(sub_df) / 2)
     best_ranked_player_win_percentage.append(lines)
+    categories_number_of_matches.append(nb_matches_lines)
 
 colors = ["purple", "blue", "cyan", "green", "yellow", "orange", "red"]
 fig, ax = plt.subplots()
@@ -68,4 +72,26 @@ plt.xlabel("Player 1 Rank Category")
 plt.ylabel("Player 2 Rank Category")
 plt.title("Best player Win percentage per Rank Category")
 plt.savefig("Best_player_win_percentage.png")
+plt.show()
+
+
+fig, ax = plt.subplots()
+
+for i, val1 in enumerate(categories_number_of_matches):
+    for j, val2 in enumerate(val1):
+        color = colors[int(val2**0.5 / np.max(categories_number_of_matches)**0.5 * (len(colors) - 1))]
+        rect = plt.Rectangle((i, j), 1, 1, fc=color)
+        ax.add_patch(rect)
+        plt.text(i + 0.2, j + 0.35, np.round(val2 * 100, 2))
+
+for i in range(len(categories)):
+    plt.plot([i, i], [0, len(categories) - 1], c="k")
+    plt.plot([0, len(categories) - 1], [i, i], c="k")
+
+plt.xticks(list(range(len(categories))), labels=categories)
+plt.yticks(list(range(len(categories))), labels=categories)
+plt.xlabel("Player 1 Rank Category")
+plt.ylabel("Player 2 Rank Category")
+plt.title("Number of matches recorded per Rank Category")
+plt.savefig("nb_matches.png")
 plt.show()
