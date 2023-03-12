1fdee68eb9982dcfe11bbdffdfa122dfeb490be4
VincentAuriau <auriau.vincent@gmail.com>
2023-03-13 00:29:47 +0100

    update example

--- examples/train_test_eval.py
+++ examples/train_test_eval.py
@@ -13,12 +13,13 @@ from data.data_loader import encode_data
 from evaluation.train_test import train_test_evaluation
 
 
-train_years = [2022]
-test_years = [2023]
+train_years = [2020, 2021]
+test_years = [2022, 2023]
 model_class = RandomForestClassifier
 model_params = {"n_estimators": 2000, "max_depth": None}
 match_features = []
 player_features = ["Ranking"]
+additional_features = ["diff_rank"]
 
 test_score = train_test_evaluation(
     train_years=train_years,
@@ -28,6 +29,73 @@ test_score = train_test_evaluation(
     match_features=match_features,
     player_features=player_features,
     encoding_params={},
+    additional_features=additional_features
 )
 
 print("Test Score", test_score)
+
+
+train_years = [2020, 2021]
+test_years = [2022, 2023]
+model_class = RandomForestClassifier
+model_params = {"n_estimators": 2000, "max_depth": None}
+match_features = []
+player_features = ["Ranking"]
+additional_features = []
+
+test_score = train_test_evaluation(
+    train_years=train_years,
+    test_years=test_years,
+    model_class=model_class,
+    model_params=model_params,
+    match_features=match_features,
+    player_features=player_features,
+    encoding_params={},
+    additional_features=additional_features
+)
+
+print("Test Score", test_score)
+
+
+train_years = [2020, 2021]
+test_years = [2022, 2023]
+model_class = RandomForestClassifier
+model_params = {"n_estimators": 2000, "max_depth": None}
+match_features = []
+player_features = []
+additional_features = ["diff_rank"]
+
+test_score = train_test_evaluation(
+    train_years=train_years,
+    test_years=test_years,
+    model_class=model_class,
+    model_params=model_params,
+    match_features=match_features,
+    player_features=player_features,
+    encoding_params={},
+    additional_features=additional_features
+)
+
+print("Test Score", test_score)
+
+train_years = [2020, 2021]
+test_years = [2022, 2023]
+model_class = RandomForestClassifier
+model_params = {"n_estimators": 1, "max_depth": 1}
+match_features = []
+player_features = []
+additional_features = ["diff_rank"]
+
+test_score = train_test_evaluation(
+    train_years=train_years,
+    test_years=test_years,
+    model_class=model_class,
+    model_params=model_params,
+    match_features=match_features,
+    player_features=player_features,
+    encoding_params={},
+    additional_features=additional_features
+)
+
+print("Test Score", test_score)
+
