ab1d8cdd1b7488f80f23f85c8bfae2b5453f1412
VincentAuriau <auriau.vincent@gmail.com>
2023-03-19 12:10:36 +0100

    add 1st version of deep model

--- python/model/base_model.py
+++ python/model/base_model.py
@@ -12,3 +12,13 @@ class BaseModel:
     @abstractmethod
     def predict(self, X):
         pass
+
+
+class DeepBaseModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.instantiate_model()
+
+    @abstractmethod
+    def instantiate_model(self, X):
+        pass
