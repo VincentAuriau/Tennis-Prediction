888258e5a0805b44104510bd157e596dc7184874
VincentAuriau <auriau.vincent@gmail.com>
2023-03-31 20:55:26 +0200

    adjust models comparison plot for better clarity

--- examples/results_reading/models_comparison.py
+++ examples/results_reading/models_comparison.py
@@ -21,19 +21,21 @@ for i, model in enumerate(df_results.model_class.unique()):
     ][i]
 fig, ax = plt.subplots()
 for n_row, row in df_results.iterrows():
-    rect = Rectangle(
-        (n_row, 0),
-        1,
-        row["precision"],
-        edgecolor=models_color[row["model_class"]],
-        facecolor=models_color[row["model_class"]],
-        label=row["model_class"],
-    )
-    ax.add_patch(rect)
+    if n_row < 200:
+        rect = Rectangle(
+            (n_row, 0),
+            1,
+            row["precision"]*100,
+            edgecolor=models_color[row["model_class"]],
+            facecolor=models_color[row["model_class"]],
+            label=row["model_class"],
+        )
+        ax.add_patch(rect)
 
 ax.autoscale()
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
-plt.legend(by_label.values(), by_label.keys())
+plt.legend(by_label.values(), by_label.keys(), loc=1)
+plt.ylabel("Precision %")
 plt.savefig("models_performances.png")
 plt.show()
