75d4a44d3a194c628943025d8a4a7d47e699097a
VincentAuriau <auriau.vincent@gmail.com>
2023-04-01 22:57:33 +0200

    fix inputs types if needed

--- python/data/data_utils.py
+++ python/data/data_utils.py
@@ -1,6 +1,7 @@
 
 
 def get_days_difference(prev_date, curr_date):
+    prev_date, curr_date = str(prev_date), str(curr_date)
     days_difference = (
                 (int(curr_date[:4]) - int(prev_date[:4]))
                 * 365
